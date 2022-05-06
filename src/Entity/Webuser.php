6c5be48a99f804c5796a1d21f12780d45a357f88
rpinformatica <azay@rpinformatica.com>
2022-05-06 15:39:46 +0200

    new

--- src/Entity/Webuser.php
+++ src/Entity/Webuser.php
@@ -60,8 +60,7 @@ class Webuser implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
+        //$roles[] = 'ROLE_USER';
         return array_unique($roles);
     }
 
